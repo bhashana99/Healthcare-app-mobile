52429bb9013ef2b4c028aa43d9427c49a1e205c6
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-03-06 12:53:30 +0530

    find doctor activity back function to home activity

--- app/src/main/java/com/example/healthcare/FindDoctorActivity.java
+++ app/src/main/java/com/example/healthcare/FindDoctorActivity.java
@@ -1,8 +1,11 @@
 package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FindDoctorActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class FindDoctorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_doctor);
+
+        CardView back=findViewById(R.id.cardFDBack);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FindDoctorActivity.this,HomeActivity.class));
+            }
+        });
     }
 }

e7e8d3de0222cc3da31bff18c0a39172c4e240b2
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-03-06 11:37:53 +0530

    create welcome message in home activity

--- app/src/main/java/com/example/healthcare/HomeActivity.java
+++ app/src/main/java/com/example/healthcare/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
+
+        SharedPreferences sharedPreferences=getSharedPreferences("shared_pref", Context.MODE_PRIVATE);
+        String username=sharedPreferences.getString("username","").toString();
+        Toast.makeText(getApplicationContext(),"Welcome "+username,Toast.LENGTH_LONG).show();
+          }
 }

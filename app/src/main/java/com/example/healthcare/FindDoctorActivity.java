28fb119b7581694692963cda601e23e418d9ca81
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-03-06 13:11:34 +0530

    find doctor activity rest of card event

--- app/src/main/java/com/example/healthcare/FindDoctorActivity.java
+++ app/src/main/java/com/example/healthcare/FindDoctorActivity.java
@@ -32,5 +32,45 @@ public class FindDoctorActivity extends AppCompatActivity {
                 startActivity(it);
             }
         });
+
+        CardView dietician = findViewById(R.id.cardFDDietician);
+        dietician.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it=new Intent(FindDoctorActivity.this,DoctorDetailsActivity.class);
+                it.putExtra("title","Dietician");
+                startActivity(it);
+            }
+        });
+
+        CardView dentist = findViewById(R.id.cardFDDentist);
+        dentist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it=new Intent(FindDoctorActivity.this,DoctorDetailsActivity.class);
+                it.putExtra("title","Dentist");
+                startActivity(it);
+            }
+        });
+
+        CardView surgeon = findViewById(R.id.cardFDSurgeon);
+        surgeon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it=new Intent(FindDoctorActivity.this,DoctorDetailsActivity.class);
+                it.putExtra("title","Surgeon");
+                startActivity(it);
+            }
+        });
+
+        CardView cardiologists = findViewById(R.id.cardFDCardiologists);
+        cardiologists.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it=new Intent(FindDoctorActivity.this,DoctorDetailsActivity.class);
+                it.putExtra("title","Cardiologists");
+                startActivity(it);
+            }
+        });
     }
 }

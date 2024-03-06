c90a8d25d198c9e219369de0263a1e2854382097
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-03-06 11:46:52 +0530

    logout functionality

--- app/src/main/java/com/example/healthcare/HomeActivity.java
+++ app/src/main/java/com/example/healthcare/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
@@ -17,5 +20,17 @@ public class HomeActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences=getSharedPreferences("shared_pref", Context.MODE_PRIVATE);
         String username=sharedPreferences.getString("username","").toString();
         Toast.makeText(getApplicationContext(),"Welcome "+username,Toast.LENGTH_LONG).show();
+
+        CardView exit=findViewById(R.id.cardExit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor= sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+                Toast.makeText(getApplicationContext(),"Logout success",Toast.LENGTH_SHORT).show();
+            }
+        });
           }
 }
